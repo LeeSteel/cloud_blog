3b99bede39820500f73686f81a9108da682f17a9
李钢 <2580704698@qq.com>
2019-08-19 23:29:16 +0800

    完善样例

--- 程序源码/后端源码/cloud-blog-simple-demo/src/test/java/com/lgcy/blog/cloudblog/CloudBlogDemoApplicationTests.java
+++ 程序源码/后端源码/cloud-blog-simple-demo/src/test/java/com/lgcy/blog/cloudblog/CloudBlogDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lgcy.blog.cloudblogparent;
+package com.lgcy.blog.cloudblog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudBlogParentApplicationTests {
+public class CloudBlogDemoApplicationTests {
 
     @Test
     public void contextLoads() {
