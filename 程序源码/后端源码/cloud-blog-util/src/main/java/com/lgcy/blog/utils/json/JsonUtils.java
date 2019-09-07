95903d6e0e846e29d4f8b625b2686f7de77cc3d2
李钢 <2580704698@qq.com>
2019-09-07 11:27:29 +0800

    时间工具类补充工具方法，json工具类去掉无用注释

--- 程序源码/后端源码/cloud-blog-util/src/main/java/com/lgcy/blog/utils/json/JsonUtils.java
+++ 程序源码/后端源码/cloud-blog-util/src/main/java/com/lgcy/blog/utils/json/JsonUtils.java
@@ -1,19 +1,11 @@
 package com.lgcy.blog.utils.json;
 
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -25,8 +17,6 @@ import net.sf.json.JSONObject;
 * @date 2019年9月2日
 *
  */
-@RequestMapping("/jsonUtils")
-@Controller
 public class JsonUtils<T> {
 	
 	/**
